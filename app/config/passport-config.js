89d45e710ef40100fcd84d3672fecf6789ae6fe9
antoniogisondi <anto.gisondi@tiscali.it>
2023-12-14 12:05:05 +0100

    Implement login with google

--- app/config/passport-config.js
+++ app/config/passport-config.js
@@ -1,9 +1,36 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const DB = require('./db-connection');
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
 
+
+// STRATEGIA PER IL LOGIN CON GOOGLE
+passport.use(new GoogleStrategy(
+    {
+        clientID: '689670408855-ood1e773s2on2bf2105ckm5lgv0oavt3.apps.googleusercontent.com',
+        clientSecret: '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',
+        callbackURL: '/google-auth-redirect',
+        responseType: 'code'
+    }, async (accessToken, refreshToken, tokenID, done) => {
+        let user = await DB.userCollection.findOne({ googleID: tokenID.id })
+        if (!user) {
+            const newUser = {
+                googleID: tokenID.id,
+                username: tokenID.username,
+                email: tokenID.emails[0].value
+            }
+
+            const ris = DB.userCollection.insertOne(newUser)
+            console.log(ris)
+        }
+
+        return done(null, user)
+    }
+))
+
+// STRATEGIA PER IL LOGIN CON LE CREDENZIALI
 passport.use(
     'local-login',
     new LocalStrategy(
@@ -17,10 +44,12 @@ passport.use(
         })
 );
 
+// SERIALIZZAZIONE DELL'UTENTE
 passport.serializeUser((user, done) => {
     done(null, user._id);
 });
 
+// DESERIALIZZAZIONE DELL'UTENTE
 passport.deserializeUser(async (id, done) => {
     // Recupero dell'utente nel database
     const user = await DB.userCollection.findOne({ _id: ObjectID(id) });
--- app/routes/login.js
+++ app/routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/passport-config');
 
+// ROTTE PER IL LOGIN GET E POST
 router.get('/login', (req, res) => {
     if (req.isAuthenticated()) return res.redirect('/user/dashboard');
     res.render('login', { message: req.flash('loginFallito') });
@@ -12,6 +13,15 @@ router.post('/login', passport.authenticate('local-login', {
     failureRedirect: '/login',
     failureFlash: true
 }));
+
+// ROTTE PER IL LOGIN CON GOOGLE
+router.get('/google-auth', passport.authenticate('google', {
+    scope: ['openid', 'email']
+}))
+
+router.get('/google-auth-redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/user/dashboard')
+})
 
 router.get('/logout', (req, res) => {
     req.logout((err) => {
